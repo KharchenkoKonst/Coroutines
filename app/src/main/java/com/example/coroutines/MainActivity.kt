0e45712bdb61e601b6883f2475700cdf9cc65750
KharchenkoKA <kharshenko.kos@gmail.com>
2021-10-08 11:02:45 +0300

    initial commit

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.coroutines
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.util.Log
 import android.widget.TextView
 import androidx.lifecycle.lifecycleScope
@@ -11,21 +12,46 @@ import kotlinx.coroutines.*
 const val TAG = "testCoroutines"
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var out: TextView
     private lateinit var counter: TextView
+
+    private suspend fun coroutineWithException() {
+        delay(100)
+        printToScreen("in")
+        throw Exception("some error")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         out = findViewById(R.id.output)
+        out.movementMethod = ScrollingMovementMethod()
         counter = findViewById(R.id.counter)
 
+        val scope = CoroutineScope(Job() + Dispatchers.Main)
+
+        lifecycleScope.launch {
+            try {
+                launch {
+                    coroutineWithException()
+                }
+            } catch (e: Exception) {
+                printToScreen("catch")
+            }
+
+        }
+
+        /**
+         * Глобальная корутина между активити
+         */
         GlobalScope.launch {
             while (true) {
                 delay(5000)
                 Log.i(TAG, "global")
             }
         }
+
         lifecycleScope.launch {
             while (true) {
                 delay(5000)
@@ -39,20 +65,27 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun runBlockingClick(view: android.view.View) {
-        /**
-         * runBlocking означает, что вызывающий поток блокируется на время вызова сопрограммы
-         */
+        // runBlocking означает, что вызывающий поток блокируется на время вызова сопрограммы
+        // Здесь выполнение идёт на main-thread, значит он будет приостановлен - вывод внутри блока произойдёт раньше
         runBlocking {
-            usefulUnitWork()
-            printToScreen("blocking callback")
+            delay(5000)
+            printToScreen("${Thread.currentThread().name} - in blocking")
+        }
+        printToScreen("${Thread.currentThread().name} - out of blocking")
+        printToScreen("")
+
+        // withContext - аналог async/await, приостанавливающий внешний scope без вызова await
+        MainScope().launch {
+            withContext(Dispatchers.IO) {
+                delay(2000)
+                printToScreen("${Thread.currentThread().name} - in new context")
+            }
+            printToScreen("${Thread.currentThread().name} - out of new context")
         }
     }
 
     fun runAsyncClick(view: android.view.View) {
-        lifecycleScope.launch {
-            usefulUnitWork()
-            printToScreen("async callback")
-        }
+
         lifecycleScope.launch {
             launch {
                 val job = launch { usefulResultWork() }
@@ -65,7 +98,6 @@ class MainActivity : AppCompatActivity() {
             launch {
                 printToScreen("waiting for result by async: ")
                 val x = async { usefulResultWork() }
-                printToScreen("awaiting")
                 printToScreen(x.await().toString() + "by await")
             }
             delay(1000)
@@ -73,6 +105,8 @@ class MainActivity : AppCompatActivity() {
             val x = usefulResultWork()
             printToScreen(x.toString() + "just by suspend")
         }
+
+        // LazyJob
         lifecycleScope.launch {
             val lazyJob = launch(start = CoroutineStart.LAZY) {
                 printToScreen("lazy job starts")
@@ -110,7 +144,30 @@ class MainActivity : AppCompatActivity() {
         finish()
     }
 
-    fun runDispatchersTest(view: android.view.View) {
+    fun runDispatchersTestClick(view: android.view.View) {
+        lifecycleScope.launch {
+            printToScreen("Starts ${Thread.currentThread().name}")
+            printToScreen("Starts 8 Dispatchers.Default")
+            repeat(8) {
+                launch(Dispatchers.Default) {
+                    printToScreen(Thread.currentThread().name)
+                }
+            }
+            delay(100)
 
+            coroutineScope {}
+
+            val res = async { usefulResultWork() }
+            launch(Dispatchers.IO) {
+                out.text = "${out.text}${Thread.currentThread().name} change UI from IO\n"
+            }
+//                printToScreen(res.await().toString())
+            repeat(8) {
+                launch(Dispatchers.IO) {
+                    printToScreen(Thread.currentThread().name)
+                }
+            }
+        }
     }
 }
+
--- app/src/main/java/com/example/coroutines/MainActivity2.kt
+++ app/src/main/java/com/example/coroutines/MainActivity2.kt
@@ -2,10 +2,13 @@ package com.example.coroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.cancel
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        GlobalScope.cancel()
     }
 }
